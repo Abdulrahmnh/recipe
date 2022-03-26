ed811403af73fb699f3244d8d20add007ce8f554
Abdulrahman <IIabdulra7man95@gmail.com>
2022-03-26 09:35:24 +0300

    Authentication Class

---
+++ lib/core/viewModel/auth_view_model.dart
@@ -0,0 +1,109 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import '../../model/user_model.dart';
+import '../../views/home_view.dart';
+import '../services/firestore_user.dart';
+
+class AuthViewModel extends GetxController {
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FacebookLogin _facebookLogin = FacebookLogin();
+
+  late String email, password, name;
+  Rxn<User> _user = Rxn<User>();
+  String? get user => _user.value?.email;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    _user.bindStream(_auth.authStateChanges());
+  }
+
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+  }
+
+  void googleSignInMethod() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    print(googleUser);
+    GoogleSignInAuthentication? googleSignInAuthentication =
+    await googleUser?.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication?.idToken,
+      accessToken: googleSignInAuthentication?.accessToken,
+    );
+
+    await _auth.signInWithCredential(credential).then((user) {
+      saveUser(user);
+      Get.offAll(HomeView());
+    });
+  }
+
+  void facebookSignInMethod() async {
+    FacebookLoginResult result = await _facebookLogin.logIn();
+    final accessToken = result.accessToken?.token;
+
+    if (result.status == FacebookLoginStatus.success){
+      final faceCredential = FacebookAuthProvider.credential(accessToken!);
+      await _auth.signInWithCredential(faceCredential);
+    }
+  }
+
+  void signInWithEmailAndPassword() async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      Get.offAll(HomeView());
+    } catch (e) {
+      print(e.hashCode);
+      Get.snackbar(
+        'Error login account',
+        e.hashCode.toString(),
+        colorText: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void createAccountWithEmailAndPassword() async {
+    try {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) async {
+        saveUser(user);
+      });
+
+      Get.offAll(HomeView());
+    } catch (e) {
+      print(e.runtimeType);
+      Get.snackbar(
+        'Error login account',
+        e.runtimeType.toString(),
+        colorText: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void saveUser(UserCredential user) async {
+    await FireStoreUser().addUserToFireStore(UserModel(
+      userId: user.user?.uid,
+      email: user.user?.email,
+      name: name == null ? user.user?.displayName : name,
+      pic: '',
+    ));
+  }
+}
