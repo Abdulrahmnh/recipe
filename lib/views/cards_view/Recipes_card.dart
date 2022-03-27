7d2cea411da5abc709855921a7cce3c81a5f1b12
Abdulrahman <IIabdulra7man95@gmail.com>
2022-03-27 15:14:26 +0300

    Recipes class which calls in home view

---
+++ lib/views/cards_view/Recipes_card.dart
@@ -0,0 +1,136 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import '../../controller/home_controller.dart';
+
+class RecipesCard extends GetView<HomeController> {
+  const RecipesCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 500,
+        child: controller.obx(
+          (data) => ListView.separated(
+            shrinkWrap: true,
+            itemCount: data!.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {},
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * .4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 180,
+                        height: 220,
+                        alignment: Alignment.bottomCenter,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 15),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/featured2.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        // Recipe Card Info
+                        child: ClipRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
+                            child: Container(
+                              height: 80,
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Colors.black.withOpacity(0.26),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  // Recipe Title
+                                  Text(
+                                    data[index]['name'].toString(),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      height: 150 / 100,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    data[index]['description'].toString(),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      height: 150 / 100,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  // Recipe Calories and Time
+                                  Container(
+                                    margin: const EdgeInsets.only(top: 8),
+                                    child: Row(
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/fire-filled.svg',
+                                          color: Colors.white,
+                                          width: 12,
+                                          height: 12,
+                                        ),
+                                        Container(
+                                          margin:
+                                              const EdgeInsets.only(left: 5),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                data[index]['kCal'].toString(),
+                                                style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 10),
+                                              ),
+                                              const SizedBox(
+                                                width: 7,
+                                              ),
+                                              const Text(
+                                                'CAL',
+                                                style: TextStyle(
+                                                    fontSize: 10,
+                                                    color: Colors.white),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, int index) => const SizedBox(
+              width: 20,
+            ),
+          ),
+        ));
+  }
+}
