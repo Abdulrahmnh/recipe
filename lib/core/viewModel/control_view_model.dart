60d46b02d4be33d18c51b69cbdeb137128c7219f
Abdulrahman <IIabdulra7man95@gmail.com>
2022-03-28 13:28:06 +0300

    Button Navigation Bar Helper

---
+++ lib/core/viewModel/control_view_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:recipe/views/screens/profile_page.dart';
+
+import '../../views/screens/home_view.dart';
+
+class ControlViewModel extends GetxController {
+  int _navigatorValue = 0;
+
+  get navigatorValue => _navigatorValue;
+
+  Widget currentScreen = HomeView();
+
+  void changeSelectedValue(int selectedValue) {
+    _navigatorValue = selectedValue;
+    switch (selectedValue) {
+      case 0:
+        {
+          currentScreen = HomeView();
+          break;
+        }
+      case 1:
+        {
+          currentScreen = ProfilePage();
+          break;
+        }
+    }
+    update();
+  }
+}
