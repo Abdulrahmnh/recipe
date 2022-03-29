702c50676a47efca9025c1ac972da4e80cd309bd
Abdulrahman <IIabdulra7man95@gmail.com>
2022-03-29 12:33:35 +0300

    Edit Shape of button navigator bar

--- lib/controller/controller_view.dart
+++ lib/controller/controller_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:recipe/views/screens/splash_view.dart';
 import '../core/viewModel/auth_view_model.dart';
 import '../core/viewModel/control_view_model.dart';
+import '../views/cards_view/ButtonGround.dart';
 import '../views/cards_view/buttonNaviBar.dart';
 
 class ControlView extends GetWidget<AuthViewModel> {
@@ -13,8 +14,17 @@ class ControlView extends GetWidget<AuthViewModel> {
           ? SplashView()
           : GetBuilder<ControlViewModel>(
               builder: (controller) => Scaffold(
-                body: controller.currentScreen,
-                bottomNavigationBar: ButtonNaviBar(),
+                body: Stack(
+                  children: [
+                    controller.currentScreen,
+                    BottomGradientWidget(),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [ButtonNaviBar()],
+                    )
+                  ],
+                ),
+                // bottomNavigationBar: ButtonNaviBar(),
               ),
             );
     });
